6022b8c029e25d4741d103f1b3e7d4d353763d3c
d-virusss <jyg0599@gmail.com>
2021-02-08 15:51:18 +0900

    line 1번

---
+++ algorithm/LINE 2020 하반기/1번 문제.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# -*- coding: euc-kr-*-
+def solution(boxes):
+    product = [0] * 100005
+    for box in boxes:
+        product[box[0]] += 1
+        product[box[1]] += 1
+
+    pair, no_pair = 0, 0
+    for e in product:
+        if e != 0:
+            if e % 2 == 0:
+                pair += 1
+            elif e % 2 == 1:
+                no_pair += 1
+
+    return len(boxes)-pair
+
+boxes = [[1,2], [2,3], [3,1]]
+print(solution(boxes))
+
+# += 헷갈려서 시간 조금 뺏김
+# 14:35 ~ 14:50, 15분 소요
