f0573e97dfe0363d6780f6925cabc46bb8ba23a1
d-virusss <jyg0599@gmail.com>
2021-03-19 00:39:32 +0900

    fibo 패턴으로 증가

--- algorithm/기타/lv3 2xn 타일링.py
+++ algorithm/기타/lv3 2xn 타일링.py
@@ -1,4 +1,9 @@
+from itertools import combinations
 def solution(n):
-    answer = 0
-    return answer
+  first, second = 1, 1
+  for i in range(n):
+    first, second = second, first+second
+  return first%1000000007
 
+n = 60000
+print(solution(n))
